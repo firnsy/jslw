89b9576a8d0f3ca97e9edab0126254d4fa10dfe3
firnsy <firnsy@securixlive.com>
2011-07-14 15:51:01 +1000

    changed: refactored main render loop.

--- js/demo.js
+++ js/demo.js
@@ -1,40 +1,61 @@
+cache = {
+  "images": [],
+  "images_loaded": 0
+};
+
+function demo_init()
+{
+  i = new Image();
+  i.src = 'images/background.png';
+  i.onerror = function() { alert("Unable to load image: " + this.src) };
+}
+
+function demo_init_complete()
+{
+
+
+}
+
+
 function demo()
 {
+  demo_init();
+  demo_run();
+}
+
+function demo_run()
+{
   w = new Widget(null, 0, 0, 800, 480);
-  w.setBackgroundColour('rgb(128, 0, 0)');
+  w.set_root();
+  w.set_background_image('images/background.png');
+  w.set_context( $("#gui")[0].getContext("2d") );
+// w.set_background_colour('rgb(128, 0, 0)');
 
   b1 = new Widget(w, 100, 100, 100, 100);
-  b1.setBackgroundColour('rgb(0, 128, 0)');
+  b1.set_background_colour('rgb(0, 128, 0)');
   b1.setVisibility(1);
 
   b11 = new Widget(b1, 95, 10, 10, 10);
-  b11.setBackgroundColour('#000077');
+  b11.set_background_colour('#000077');
 
   b2 = new Widget(w, 250, 10, 10, 100);
-  b2.setBackgroundColour('#007f7f');
+  b2.set_background_colour('#007f7f');
 
+  t1 = new Widget(w, 304, 429, 48, 51)
+  t1.set_background_image('images/rewind_up.png');
 
-  var context = $("#gui")[0].getContext("2d");
-//  context.scale(0.375, 0.312);
+  t2 = new Widget(w, 352, 429, 48, 51)
+  t2.set_background_image('images/play_up.png');
 
-  w.render( context, 0, 0 );
+  t3 = new Widget(w, 400, 429, 48, 51)
+  t3.set_background_image('images/stop_up.png');
 
-  b1.hide();
+  t4 = new Widget(w, 448, 429, 48, 51)
+  t4.set_background_image('images/forward_up.png');
 
-  draw();
-}
+  w.update();
 
-function draw()
-{
-  var context = $("#gui")[0].getContext("2d");
-
-  w.process();
-
-  if ( w.is_dirty() )
-  {
-    w.render(context, 0, 0);
-  }
-
-  // setup the render loop
-  setTimeout(function() { draw(); }, 100);
+  setTimeout(function() { b2.slideRight() } , 2000);
+//  setTimeout(function() { b11.slideLeft() } , 2500);
+//  setTimeout(function() { b1.slideDown() } , 2750);
 }
--- js/widget.js
+++ js/widget.js
@@ -1,10 +1,44 @@
+Vector2 = function(x, y)
+{
+  this.set(x, y);
+}
+
+Vector2.prototype.set = function(x, y)
+{
+  this.x = x + 0;
+  this.y = y + 0;
+}
+
+Vector2.prototype.shiftX = function(x)
+{
+  this.x += x;
+}
+
+Vector2.prototype.shiftY = function(y)
+{
+  this.y += y;
+}
+
+Vector2.prototype.translate = function(v)
+{
+  this.x += v.x;
+  this.y += v.y;
+}
+
+Vector2.prototype.scale = function(s)
+{
+  this.x *= s;
+  this.y *= s;
+}
+
+
 // rectangle object description
 Rect = function(x, y, w, h)
 {
-  this.x = x;       // x coordinate for top-left
-  this.y = y;       // y coordinate for top-left
-  this.w = w;
-  this.h = h;
+  this.x = x + 0;       // x coordinate for top-left
+  this.y = y + 0;       // y coordinate for top-left
+  this.w = w + 0;
+  this.h = h + 0;
   this.x2 = x + w;  // x coordinate for bottom-right
   this.y2 = y + h;  // y coordinate for bottom-right
 }
@@ -21,49 +55,80 @@ Rect.prototype.setRect = function(r)
 
 Rect.prototype.offset = function(x, y)
 {
-  this.x += x;
-  this.y += y;
-  this.x2 += x;
-  this.y2 += y;
+  this.x += x + 0;
+  this.y += y + 0;
+  this.x2 += x + 0;
+  this.y2 += y + 0;
 }
 
+Rect.prototype.pointIntersects = function(x, y)
+{
+  return ( x >= this.x && x <= this.x2 &&
+           y >= this.y && y <= this.y2 );
+}
+
+Rect.prototype.vectorIntersects= function(v)
+{
+  this.pointIntersects(v.x, v.y);
+}
 
 // implementation
 
 Widget = function(p, x, y, w, h, c) {
 
   this.bounds = new Rect(x, y, w, h);
+  this.offset = new Vector2(0, 0);
   this.bgcolour = "rgba(128, 128, 128, 0.5)";
   this.bgimage = new Image();
+  this.parent = p;
+  this.root = false;
   this.children = [];
   this.visible = true;
-  this.root = 1;  // root widget
 
-  this.visible_transition = 0;
-  this.visible_transition_frames = 10;
-  this.visible_transition_index = 0;
-  this.visible_transition_delta_x = 0;
-  this.visible_transition_delta_y = 0;
-  this.visible_transition_bounds = new Rect();
-  this.visible_transition_end_state = 0;
+  this.animate = 0;
+  this.animate_frames = 50;
+
+  // rendering members
+  this.updating = false;
+  this.context = null;
+
 
   // add to parent object if appropriate
   if( p instanceof Widget )
   {
-    p.addChild(this);
+    p.add_child(this);
   }
-  else if ( p == null )
-  {
-    // add mouse handling
 
-    this.root = 1;
-  }
+  this.dirty = true;
 }
 
-Widget.prototype.addChild = function(child)
+Widget.prototype.set_root = function()
+{
+  // handles all
+  this.root = true;
+
+}
+
+Widget.prototype.is_root = function()
+{
+  return this.root;
+}
+
+Widget.prototype.get_root = function()
+{
+  if ( this.parent == null )
+    return this;
+
+  return this.parent.get_root();
+}
+
+
+Widget.prototype.add_child = function(child)
 {
   this.children.push(child);
 }
+
+// VISIBILITY AND TRANSITION
 
 Widget.prototype.setVisibility = function(state)
 {
@@ -72,26 +137,122 @@ Widget.prototype.setVisibility = function(state)
 
 Widget.prototype.hide = function()
 {
-  this.visible_transition = 1;
-  this.visible_transition_index = 0;
-  this.visible_transition_delta_x = -(this.bounds.x2 / this.visible_transition_frames);
-  this.visible_transition_bounds.setRect(this.bounds);
-  this.visible_transition_end_state = 0;
+  this.visible = false;
+  this.dirty = true;
+  this.animate = 1;
+  this.animate_index = this.animate_frames - 1;
+  this.animate_end_state = 0;
+//  this.slideLeft();
+//  this.slideRight();
+//  this.slideUp();
+//  this.slideDown();
 }
 
 Widget.prototype.show = function()
 {
-  this.dirty = true;
+  this.offset.set(0, 0);
+  this.visible = true;
+
+  this.make_dirty();
 }
 
-Widget.prototype.setBackgroundImage = function(path)
+Widget.prototype.slideLeft = function()
 {
+  this.animate = 1;
+  this.animate_index = 0;
+  this.animate_delta_v = new Vector2(
+    -(this.bounds.x2 / this.animate_frames),
+    0
+  );
+  this.animate_end_state = 0;
 
+  this.make_dirty();
 }
 
-Widget.prototype.setBackgroundColour = function(colour)
+Widget.prototype.slideRight = function()
+{
+  this.animate = 1;
+  this.animate_index = 0;
+  this.animate_delta_v = new Vector2(
+    (this.parent.bounds.w - this.bounds.x) / this.animate_frames,
+    0
+  );
+  this.animate_end_state = ( ! this.visible );
+  this.visible = true;
+
+  this.make_dirty();
+}
+
+Widget.prototype.slideUp = function()
+{
+  this.animate = 1;
+  this.animate_index = 0;
+  this.animate_delta_v = new Vector2(
+    0,
+    -(this.bounds.y2 / this.animate_frames)
+  );
+  this.animate_end_state = 0;
+
+  this.make_dirty();
+}
+
+Widget.prototype.slideDown = function()
+{
+  this.animate = 1;
+  this.animate_index = 0;
+  this.animate_delta_v = new Vector2(
+    0,
+    (this.parent.bounds.h - this.bounds.y) / this.animate_frames
+  );
+  this.animate_end_state = 0;
+
+  this.make_dirty();
+}
+
+
+
+
+
+
+// STYLING
+
+Widget.prototype.set_background_image = function(path)
+{
+  this.bgimage.src = path;
+  this.bgimage.onerror = function(){ alert("Unable to load image: " + this.src); };
+
+  var this_object = this;
+  this.bgimage.onload = function() { this_object.dirty = true; };
+}
+
+Widget.prototype.set_background_colour = function(colour)
 {
   this.bgcolour = colour;
+}
+
+// PROCESS AND RENDERING
+
+Widget.prototype.make_dirty = function()
+{
+  this.dirty = true;
+
+  this.get_root().update();
+}
+
+Widget.prototype.make_clean = function()
+{
+  this.dirty = false;
+}
+
+Widget.prototype.set_context = function(context)
+{
+  if( ! ( context instanceof CanvasRenderingContext2D ) )
+  {
+    alert("You have not supplied the correct context.");
+    return;
+  }
+
+  this.context = context;
 }
 
 Widget.prototype.is_dirty = function()
@@ -112,19 +273,30 @@ Widget.prototype.process = function()
   if( ! this.visible )
     return;
 
-
-  if ( this.visible_transition )
+  // are we animating
+  if ( this.animate )
   {
-    this.visible_transition_index++;
+    this.animate_index++;
 
-//    if( this.visible_transition_index >= this.visible_transition_frames )
-  //    this.visible_transition = 0;
+    this.offset.translate(this.animate_delta_v);
+
+    if( this.animate_index > this.animate_frames )
+    {
+      this.visible = this.animate_end_state;
+      this.animate = 0;
+    }
 
     this.dirty = true;
   }
 
+  // track dirty states of children
+  var is_dirty = this.dirty
+
   for( c in this.children )
-    this.children[c].process();
+    is_dirty |= this.children[c].process();
+
+  // return aggregate dirty state
+  return is_dirty;
 }
 
 Widget.prototype.render = function(context, x, y)
@@ -133,26 +305,15 @@ Widget.prototype.render = function(context, x, y)
     return;
 
   if( ! this.visible )
+  {
     return;
+  }
 
   // save our context
   context.save();
 
-  // all children objects are draw relative to parent
-  if ( this.visible_transition )
-  {
-//    this.visible_transition_index++;
-
-    context.translate(x + (this.visible_transition_index * this.visible_transition_delta_x), y + (this.visible_transition_index * this.visible_transition_delta_y));
-
-    if( this.visible_transition_index >= this.visible_transition_frames )
-    {
-      this.visible_transition = 0;
-      this.visible = this.visibile_transition_end_state;
-    }
-  }
-  else
-    context.translate(x, y);
+  // offset the view (accounts for animations)
+  context.translate(x + this.offset.x, y + this.offset.y);
 
   // all children must be confined to parents bounds
   context.beginPath();
@@ -160,10 +321,10 @@ Widget.prototype.render = function(context, x, y)
   context.clip();
   context.closePath();
 
+  // draw the widget
   if( this.bgimage.src != "" && this.bgimage.complete )
-  //if( 0 )
   {
-    context.drawImage(this.bgimage, this.bounds.x, this.bounds.y, this.bounds.w, this.bounds.y);
+    context.drawImage(this.bgimage, this.bounds.x, this.bounds.y, this.bounds.w, this.bounds.h);
   }
   else
   {
@@ -179,3 +340,30 @@ Widget.prototype.render = function(context, x, y)
   this.dirty = false;
 }
 
+Widget.prototype.update = function()
+{
+  if ( this.updating || ! this.is_root() )
+  {
+    alert("I'm working.");
+    return;
+  }
+
+  this.updating = true;
+
+  var is_dirty = this.process();
+
+
+  if ( is_dirty )
+  {
+    this.context.save();
+
+    w.render(this.context, 0, 0);
+
+    this.context.restore();
+
+    // setup the render loop
+    setTimeout( ( function(scope) { return function() { scope.update(); }; } )(this), 200);
+  }
+
+  this.updating = false;
+}
